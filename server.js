a58c0f5f7582309034247b6128ac1fcfa0f7bc15
Borasta <orlandodavid.01@gmail.com>
2016-07-17 16:51:58 -0400

    agregada ruta estatica

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ var express = require("express");
 
 var app = express();
 
+app.use(express.static(__dirname + "/www/"));
+
 app.listen(5000, function () {
     console.log("Server iniciado")
-});
-
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/www/index.html");
 });
